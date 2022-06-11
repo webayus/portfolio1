dbd38895d7e0d5b42b5e11685388e4a8609cb56c
OmBayus <omerbayramcavus@gmail.com>
2022-06-11 21:36:54 +0300

    Contacts Reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import aboutReducer from "../reducers/about/aboutSlice";
 import blogReducer from "../reducers/blog/blogSlice";
 import achievementReducer from "../reducers/achievement/achievementSlice";
+import contactsReducer from "../reducers/contacts/contactsSlice";
 
 export default configureStore({
   reducer: {
     about: aboutReducer,
     achievement: achievementReducer,
-    blog: blogReducer
+    blog: blogReducer,
+    contacts: contactsReducer,
   },
 });
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -18,12 +18,13 @@ import {
 } from "../../components";
 import { headerData } from "../../data/headerData";
 
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 
 function Main() {
   const about = useSelector((state) => state.about);
   const blog = useSelector((state) => state.blog);
   const achievement = useSelector((state) => state.achievement);
+  const contacts = useSelector((state) => state.contacts);
 
   return (
     <div>
@@ -31,18 +32,26 @@ function Main() {
         <title>{headerData.name} - Porfolio</title>
       </Helmet>
 
-      <Navbar about={about.visible} education={true} services={true} contacts={true} blog={blog.visible} />
+      <Navbar
+        about={about.visible}
+        education={true}
+        services={true}
+        contacts={contacts.visible}
+        blog={blog.visible}
+      />
       <Landing />
       {about.visible ? <About aboutData={about.data} /> : null}
       <Education />
       <Skills />
       <Experience />
       <Projects />
-      {achievement.visible ? <Achievement achievementData={achievement.data}  /> : null}
+      {achievement.visible ? (
+        <Achievement achievementData={achievement.data} />
+      ) : null}
       <Services />
       <Testimonials />
       {blog.visible ? <Blog blogData={blog.data} /> : null}
-      <Contacts />
+      {contacts.visible ? <Contacts /> : null}
       <Footer />
     </div>
   );
